e78efb02c76b7c69b3b96a6692ade0acc436cb22
Sorel <soreltsofack6@gmail.com>
2026-05-22 08:40:49 +0100

    modification de test dans securite.py

--- src/moniteur.py
+++ src/moniteur.py
@@ -1,5 +1,6 @@
 from equipements import Equipement
 from paquets import Paquet
+
 class MoniteurReseau:
     def __init__(self):
         self.equipements = []
@@ -10,7 +11,7 @@ class MoniteurReseau:
         self.equipements.append(equipement)
 
     def enregistrer_paquet(self, paquet):
-        nom = paquet.source_ip
+        nom = paquet.source
         if nom not in self.stats_paquets:
             self.stats_paquets[nom] = {"envoyes": 0, "perdus": 0}
 
@@ -24,13 +25,13 @@ class MoniteurReseau:
             self.historique.pop(0)
 
     def afficher_equipements(self):
-        print("\n=== Equipements actifs/inactifs ===")
+        print("\n Equipements actifs/inactifs ")
         for eq in self.equipements:
             info = eq.diagnostiquer()
             print(info["nom"], "| IP:", info["ip"], "| Statut:", info["statut"])
 
     def afficher_stats(self):
-        print("\n=== Statistiques paquets ===")
+        print("\n Statistiques paquets ")
         for ip, stat in self.stats_paquets.items():
             total = stat["envoyes"] + stat["perdus"]
             if total > 0:
@@ -40,30 +41,64 @@ class MoniteurReseau:
             print("IP:", ip, "| Envoyés:", stat["envoyes"], "| Perdus:", stat["perdus"], "| Taux perte:", taux, "%")
 
     def afficher_historique(self):
-        print("\n=== 10 derniers paquets ===")
+        print("\n 10 derniers paquets ")
         for paquet in self.historique:
-            print(paquet.source_ip, "-->", paquet.dest_ip, "| Statut:", paquet.statut)
+            print(paquet.source, "-->", paquet.destination, "| Statut:", paquet.statut)
 
     def generer_rapport(self):
         fichier = open("rapport_simnet.txt", "w")
-        fichier.write("=== Rapport SimNet ===\n\n")
+        fichier.write(" Rapport SimNet \n\n")
 
-        fichier.write("=== Equipements ===\n")
+        fichier.write(" Equipements \n")
         for eq in self.equipements:
             info = eq.diagnostiquer()
             fichier.write("Nom: " + info["nom"] + " | IP: " + info["ip"] + " | Statut: " + info["statut"] + "\n")
 
-        fichier.write("\n=== Statistiques paquets ===\n")
+        fichier.write("\n Statistiques paquets \n")
         for ip, stat in self.stats_paquets.items():
             fichier.write("IP: " + ip + "\n")
             fichier.write("Envoyes: " + str(stat["envoyes"]) + "\n")
             fichier.write("Perdus: " + str(stat["perdus"]) + "\n\n")
 
-        fichier.write("=== 10 derniers paquets ===\n")
+        fichier.write(" 10 derniers paquets \n")
         for paquet in self.historique:
-            fichier.write(paquet.source_ip + " --> " + paquet.dest_ip + " : " + paquet.statut + "\n")
+            fichier.write(paquet.source + " --> " + paquet.destination + " : " + paquet.statut + "\n")
 
         fichier.close()
         print("Rapport généré : rapport_simnet.txt")
 
 
+
+
+if __name__ == "__main__":
+    
+    
+    from paquets import Paquet
+
+    moniteur = MoniteurReseau()
+
+    eq1 = Equipement("Routeur1", "Cisco", "192.168.1.1")
+    eq2 = Equipement("Switch1", "HP", "192.168.1.2")
+    eq3 = Equipement("Serveur1", "Dell", "192.168.1.3")
+
+    eq1.activer()
+    eq2.activer()
+
+    moniteur.ajouter_equipement(eq1)
+    moniteur.ajouter_equipement(eq2)
+    moniteur.ajouter_equipement(eq3)
+
+    p1 = Paquet("192.168.1.1", "10.0.0.1", "TCP", 500, 2, "TRANSMIS")
+    p2 = Paquet("192.168.1.1", "10.0.0.2", "UDP", 200, 1, "PERDU")
+    p3 = Paquet("192.168.1.2", "10.0.0.3", "ICMP", 100, 3, "TRANSMIS")
+    p4 = Paquet("192.168.1.2", "10.0.0.4", "TCP", 300, 1, "PERDU")
+
+    moniteur.enregistrer_paquet(p1)
+    moniteur.enregistrer_paquet(p2)
+    moniteur.enregistrer_paquet(p3)
+    moniteur.enregistrer_paquet(p4)
+
+    moniteur.afficher_equipements()
+    moniteur.afficher_stats()
+    moniteur.afficher_historique()
+    moniteur.generer_rapport()
--- src/securite.py
+++ src/securite.py
@@ -41,22 +41,34 @@ class GestionFirewall:
             print(log)
 
 
-#  TEST 
-from equipements import Firewall
-from paquets import Paquet
+if __name__ == "__main__":
 
-fw = Firewall("FW1", "192.168.1.254", "Cisco", "actif", "admin", "1234")
-gestion = GestionFirewall(fw)
+    from equipements import Firewall
+    from paquets import Paquet
 
-gestion.ajouter_regle("admin", "1234", "bloquer", "192.168.1.1", "TCP", 80)
-gestion.ajouter_regle("admin", "0000", "bloquer", "192.168.1.2", "UDP", 443)
 
-p1 = Paquet("192.168.1.1", "10.0.0.1", "TCP", 500, 2)
-p2 = Paquet("192.168.1.2", "10.0.0.2", "UDP", 200, 1)
-p3 = Paquet("192.168.1.5", "10.0.0.3", "ICMP", 100, 3)
+    print(" Creation du firewall ")
+    fw = Firewall("FW1", "192.168.1.254", "Cisco", "actif", "admin", "1234")
+    gestion = GestionFirewall(fw)
+    print("Firewall:", fw.nom, "| IP:", fw.ip)
 
-gestion.inspecter_paquet(p1)
-gestion.inspecter_paquet(p2)
-gestion.inspecter_paquet(p3)
+    print("\n Authentification ")
+    print("admin / 1234 :", gestion.authentifier("admin", "1234"))
+    print("admin / 0000 :", gestion.authentifier("admin", "0000"))
 
-gestion.afficher_journal()
+    print("\n Ajouter_regle ")
+    gestion.ajouter_regle("admin", "1234", "bloquer", "192.168.1.1", "TCP", 80)
+    gestion.ajouter_regle("admin", "1234", "bloquer", "192.168.1.2", "UDP", 443)
+    gestion.ajouter_regle("admin", "0000", "autoriser", "192.168.1.3", "ICMP", 0)
+
+    print("\n Inspection de paquet ")
+    p1 = Paquet("192.168.1.1", "10.0.0.1", "TCP", 500, 2, "TRANSMIS")
+    p2 = Paquet("192.168.1.2", "10.0.0.2", "UDP", 200, 1, "TRANSMIS")
+    p3 = Paquet("192.168.1.5", "10.0.0.3", "ICMP", 100, 3, "TRANSMIS")
+
+    print("p1 :", gestion.inspecter_paquet(p1))
+    print("p2 :", gestion.inspecter_paquet(p2))
+    print("p3 :", gestion.inspecter_paquet(p3))
+
+    print("\n Affiche du journal ")
+    gestion.afficher_journal()
